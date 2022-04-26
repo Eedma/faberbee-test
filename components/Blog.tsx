0f10d08d348c83810855516db249d8b26698428b
Eedma <ed.marzorati@gmail.com>
2022-04-26 22:31:20 +0200

    fix quotes error

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -8,10 +8,10 @@ const Blog = ({ children }: BlogProps) => (
   <Container maxWidth="lg">
     <Box maxWidth="sm" pt={2} pl={2}>
       <Typography variant="h2" component="h1" gutterBottom>
-        This is the blog
+        {"This is the blog"}
       </Typography>
       <Typography variant="h5" component="h2" gutterBottom>
-        Here's some amazing content
+        {"Here's some amazing content"}
       </Typography>
     </Box>
     <Grid container spacing={4} pt={3}>
