524e710a5fbc0d6447c1ea40750ee4c967b6bc0b
Eedma <ed.marzorati@gmail.com>
2022-04-27 02:52:07 +0200

    blog title styles fix

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -7,14 +7,24 @@ type BlogProps = {
 
 const Blog: FC<BlogProps> = ({ children }) => (
   <Container maxWidth="lg">
-    <Box maxWidth="sm" pt={2} pl={2}>
-      <Typography variant="h2" component="h1" gutterBottom>
-        {"This is the blog"}
-      </Typography>
-      <Typography variant="h5" component="h2" gutterBottom>
-        {"Here's some amazing content"}
-      </Typography>
-    </Box>
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      paddingTop={4}
+      paddingBottom={2}
+    >
+      <Grid item xs={3}>
+        <Typography variant="h2" component="h1" gutterBottom textAlign="center">
+          {"This is the blog"}
+        </Typography>
+        <Typography variant="h5" component="h2" gutterBottom textAlign="center">
+          {"Here's some amazing content"}
+        </Typography>
+      </Grid>
+    </Grid>
     <Grid container spacing={4} pt={3}>
       {children}
     </Grid>
